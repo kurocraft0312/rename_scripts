924288f654269d4b626321b73ea13ace21b80131
kurocraft0312 <kurocraft0312@gmail.com>
2021-01-14 19:34:38 +0900

    add:初期構築

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rename_scripts
+
+## 「rename_scripts」でできること
+「rename_scripts」は、Google SpreadSheetに記載されているセル列の値にリネームしてくれる一括リネーム用のスクリプトです。
---
+++ rename.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ①スプレッドシートを読み込む(参照：https://hacknote.jp/archives/6948/)
+SHEET_ID='/'
+CSV_URL="https://docs.google.com/spreadsheets/d/${SHEET_ID}#gid=0"
+
+# echo "${CSV_URL}"
+
+# wget --no-check-certificate -q -O "${OUTPUT}"${CSV_URL}
+
+# cat ${OUTPUT}
+
+# ①画像があるフォルダを参照する
+
+# ②ファイル名を取得する
+
+# ③ スプレッドシートのパス名に置換する
+
